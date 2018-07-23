18181bb3ae39a075bd97e1c79cda768a007fe86a
Funvacuum <gitkraken.fk567kxa1m@pkt5.com>
2018-07-23 15:34:09 +0100

    adjusted graphing box to incorporate title and added --showplts for interactive plots

--- binGraph.py
+++ binGraph.py
@@ -18,7 +18,10 @@ import hashlib
 import statistics
 import os, re
 import json
+import sys
 
+if sys.version_info[0] < 3:
+    raise Exception('Must use Python 3')
 
 # # Helper functions
 def shannon_ent(labels, base=256):
@@ -63,9 +66,10 @@ def clean_fname(fn):
     return ''.join([c for c in fn if re.match(r'[\w\_\-]', c)])
 
 # ## Global variables
-__figformat__ = 'png'
-__figsize__ = (12,4)
-__figdpi__ = 100
+__figformat__ = 'png'   # Output format of saved figure
+__figsize__ = (12,4)    # Size of figure in inches
+__figdpi__ = 100        # DPI of figure
+__showplt__ = False     # Show the plot interactively
 
 
 # ## Byte histogram over all file
@@ -86,30 +90,30 @@ __ignore_0__ = True
 __bins__ = 1
 __g_log__ = 1
 __no_order__ = True
-def bin_hist(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figdpi=__figdpi__, ignore_0=__ignore_0__, bins=__bins__, g_log=__g_log__, ordered=__no_order__):
+def bin_hist(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figdpi=__figdpi__, ignore_0=__ignore_0__, bins=__bins__, g_log=__g_log__, ordered=__no_order__, showplt=__showplt__):
 
     if not figname:
         figname = 'bin_hist-{}.{}'.format(clean_fname(binname), frmt)
-        log.debug('No name given. Generated: {}'.format(figname))
+        log.info('No name given. Generated: {}'.format(figname))
 
     file_array = []
     with open(binname, 'rb') as fh:
         for x in fh.read():
             file_array.append(x)
 
-    log.debug('Read: "{}", length: {}'.format(binname, len(file_array)))
+    log.info('Read: "{}", length: {}'.format(binname, len(file_array)))
 
     ignore_0 = int(ignore_0)
-    log.debug('Ignore 0\'s: {}'.format(ignore_0))
+    log.info('Ignore 0\'s: {}'.format(ignore_0))
 
     fig, ax = plt.subplots(figsize=figsize, dpi=figdpi)
 
     if ignore_0:
         ax.set_xlim(1,255)
-        log.debug('Setting xlim to (1,255)')
+        log.info('Setting xlim to (1,255)')
     else:
         ax.set_xlim(0,255)
-        log.debug('Setting xlim to (0,255)')
+        log.info('Setting xlim to (0,255)')
 
     # # Add a byte hist ordered 1 > 255
     ordered_row = []
@@ -118,9 +122,9 @@ def bin_hist(binname, frmt=__figformat__, figname=None, figsize=__figsize__, fig
         ordered_row.append(c[x])
 
     ax.bar((range(ignore_0, 256)), ordered_row, bins, label='Bytes', color='r', log=g_log, zorder=0)
-    log.debug('Graphed binary array')
+    log.info('Graphed binary array')
 
-    # # Add a byte hist ordered by occurance - shows general distribution
+    # # Add a byte hist ordered by occurrence - shows general distribution
     if ordered:
         sorted_row = []
         c = Counter(file_array)
@@ -131,33 +135,36 @@ def bin_hist(binname, frmt=__figformat__, figname=None, figsize=__figsize__, fig
         sorted_row.reverse()
 
         ax.bar((range(ignore_0,256)), sorted_row, bins, label='Ordered', color='b', log=g_log, zorder=1, alpha=.5)
-        log.debug('Graphed ordered binary array')
+        log.info('Graphed ordered binary array')
 
     ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: ('0x%x') % (int(x))))
     ax.xaxis.set_major_locator(MaxNLocator(20))
     ax.set_xlabel('Bytes')
-    ax.set_ylabel('Occurance (log {})'.format(g_log))
+    ax.set_ylabel('Occurrence (log {})'.format(g_log))
 
     plt.legend(loc=(1.03, 0.9))
 
-    fig.suptitle('Byte histogram: {}'.format(os.path.basename(binname)))
+    fig.suptitle('Byte histogram: {}'.format(os.path.basename(binname)), horizontalalignment='center', verticalalignment='top')
     fig.subplots_adjust(hspace=0.5)
 
-    logo = plt.imread('cape.png')
+    logo = plt.imread(os.path.dirname(os.path.realpath(__file__))+'/cape.png')
     fig.figimage(logo, alpha=.5, zorder=99)
 
-    plt.savefig(fname=figname, format=frmt, bbox_inches='tight')
-    log.debug('Saved to: "{}"'.format(figname))
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
-    if False:
+    if showplt:
+        log.info('Opening graph interactively')
         plt.show()
+    else:
+        plt.savefig(fname=figname, format=frmt, bbox_inches='tight')
+        log.info('Saved to: "{}"'.format(figname))
 
     plt.clf()
     plt.cla()
-    plt.close(fig)
+    plt.close()
 
 
-# ## Entropy and byte occurance analysis over all file
+# ## Entropy and byte occurrence analysis over all file
 # # -------------------------------------------
 # # binname: file to load and analyse
 # # figsize: specify size of the figure ouputted
@@ -173,20 +180,20 @@ def bin_hist(binname, frmt=__figformat__, figname=None, figsize=__figsize__, fig
 __chunks__ = 750
 __ibytes__= '{"0\'s": [0], "Printable ASCII": [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126], "Exploit": [44, 144]}'
 __ibytes_dict__ = json.loads(__ibytes__)
-def bin_ent(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figdpi=__figdpi__, chunks=__chunks__, ibytes=__ibytes_dict__):
+def bin_ent(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figdpi=__figdpi__, chunks=__chunks__, ibytes=__ibytes_dict__, showplt=__showplt__):
 
     if not figname:
         figname = 'bin_ent-{}.{}'.format(clean_fname(binname), frmt)
-        log.debug('No name given. Generated: {}'.format(figname))
+        log.info('No name given. Generated: {}'.format(figname))
 
     fh = open(binname, 'rb')
-    log.debug('Opening: "{}"'.format(binname))
+    log.info('Opening: "{}"'.format(binname))
 
     # # Calculate the overall chunksize 
     fs = os.fstat(fh.fileno()).st_size
     chunksize = -(-fs // chunks)
     nr_chunksize = fs / chunks
-    log.debug('Filesize: {}, Chunksize (rounded): {}, Chunksize: {}, Chunks: {}'.format(fs, chunksize, nr_chunksize, chunks))
+    log.info('Filesize: {}, Chunksize (rounded): {}, Chunksize: {}, Chunks: {}'.format(fs, chunksize, nr_chunksize, chunks))
 
     shannon_samples = []
 
@@ -194,8 +201,8 @@ def bin_ent(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figd
     if len(ibytes) > 0:
         byte_ranges = {key: [] for key in ibytes.keys()}
 
-    log.debug('Going for iteration over bytes with chunksize {}'.format(chunksize))
-    log.debug('Using ibytes: {}'.format(ibytes))
+    log.info('Going for iteration over bytes with chunksize {}'.format(chunksize))
+    log.info('Using ibytes: {}'.format(ibytes))
 
     prev_ent = 0
     for chunk in get_chunk(fh, chunksize=chunksize):
@@ -212,14 +219,14 @@ def bin_ent(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figd
             cbytes = Counter(chunk)
             for label, b_range in ibytes.items():
 
-                occurance = 0
+                occurrence = 0
                 for b in b_range:
-                    occurance += cbytes[b]
+                    occurrence += cbytes[b]
 
-                byte_ranges[label].append((float(occurance)/float(len(chunk)))*100)
+                byte_ranges[label].append((float(occurrence)/float(len(chunk)))*100)
 
     fh.close()
-    log.debug('Closed: "{}"'.format(binname))
+    log.info('Closed: "{}"'.format(binname))
 
     # # Draw the graphs in order
     zorder=99
@@ -232,61 +239,60 @@ def bin_ent(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figd
     host.set_xlim([0, len(shannon_samples)+1])
     host.set_ylim([0, 1.05])
 
-    log.debug('Plotting shannon samples')
+    log.info('Plotting shannon samples')
     host.plot(shannon_samples, label='Entropy', c=section_colour('Entropy'), zorder=zorder, linewidth=1)
 
     host.set_ylabel('Entropy\n'.format(chunksize))
     host.set_xlabel('Raw file offset')
     host.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: ('0x%x') % (int(x * nr_chunksize)))) 
     host.xaxis.set_major_locator(MaxNLocator(10))
-    plt.xticks(rotation=-10, ha='left')
-
+    plt.xticks(rotation=-10, horizontalalignment='left')
 
     # # Plot individual byte percentages
     if len(ibytes) > 0:
-        log.debug('Plotting ibytes')
+        log.info('Plotting ibytes')
 
         axBytePc = host.twinx()
         axBytePc.set_ylim([-0.3, 101])
-        axBytePc.set_ylabel('Occurance of bytes (%)')
+        axBytePc.set_ylabel('Occurrence of bytes (%)')
         axBytePc.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: ('%i%%') % (x)))
 
         for label, percentages in byte_ranges.items():
             zorder -= zorder
             c = section_colour(label)
-            axBytePc.plot(percentages, label=label, c=c, zorder=zorder, linewidth=0.7)
+            axBytePc.plot(percentages, label=label, c=c, zorder=zorder, linewidth=0.7, alpha=0.75)
 
     # # Filetype specific additions
     try:
         exebin = lief.parse(filepath=binname)
-        log.debug('Parsed with lief as {}'.format(exebin.format))
+        log.info('Parsed with lief as {}'.format(exebin.format))
 
     except Exception as e:
         exebin = None
-        log.debug('Failed to parse with lief: {}'.format(e))
+        log.info('Failed to parse with lief: {}'.format(e))
 
     if exebin:
         if type(exebin) == lief.PE.Binary:
 
-            log.debug('Adding PE customisations')
+            log.info('Adding PE customisations')
 
             # # Entrypoint (EP) pointer and vline
             v_ep = exebin.va_to_offset(exebin.entrypoint) / nr_chunksize
             host.axvline(x=v_ep, linestyle='--', c='r')
-            host.text(x=v_ep, y=1.07, s='EP', rotation=90, verticalalignment='bottom', horizontalalignment='center')
+            host.text(x=v_ep, y=1.07, s='EP', rotation=45, verticalalignment='bottom', horizontalalignment='left')
 
             # # Section vlines
             for index, section in enumerate(exebin.sections):
 
-                log.debug('{}: {}'.format(fix_section_name(section, index), section.offset))
+                log.info('{}: {}'.format(fix_section_name(section, index), section.offset))
 
                 section_offset = section.offset / nr_chunksize
 
                 host.axvline(x=section_offset, linestyle='--')
-                host.text(x=section_offset, y=1.07, s=fix_section_name(section, index), rotation=90, verticalalignment='bottom', horizontalalignment='center')
+                host.text(x=section_offset, y=1.07, s=fix_section_name(section, index), rotation=45, verticalalignment='bottom', horizontalalignment='left')
 
         else:
-            log.debug('Not currently customised: {}'.format(exebin.format))
+            log.info('Not currently customised: {}'.format(exebin.format))
 
 
     # Add legends
@@ -298,22 +304,24 @@ def bin_ent(binname, frmt=__figformat__, figname=None, figsize=__figsize__, figd
     if len(ibytes) > 0:
         axBytePc.legend(loc=(1.1, 0.5))
 
-    logo = plt.imread('cape.png')
+    logo = plt.imread(os.path.dirname(os.path.realpath(__file__))+'/cape.png')
 
-    fig.suptitle('Binary entropy (sampled over {} byte chunks): {}'.format(chunksize, os.path.basename(binname)), y=1.1)
+    fig.suptitle('Binary entropy (sampled over {} byte chunks): {}'.format(chunksize, os.path.basename(binname)), horizontalalignment='center', verticalalignment='top', y=0.93)
     fig.subplots_adjust(hspace=0.5)
-
     fig.figimage(logo, alpha=.5, zorder=99)
 
-    plt.savefig(fname=figname, format=frmt, bbox_inches='tight')
-    log.debug('Saved to: "{}"'.format(figname))
+    fig.tight_layout(rect=[0, 0, 1, 0.76])
 
-    if False:
+    if showplt:
+        log.info('Opening graph interactively')
         plt.show()
+    else:
+        plt.savefig(fname=figname, format=frmt, bbox_inches='tight')
+        log.info('Saved to: "{}"'.format(figname))
 
     plt.clf()
     plt.cla()
-    plt.close(fig)
+    plt.close()
 
 
 def section_graphs():
@@ -388,7 +396,7 @@ def section_graphs():
     # # bins int:      Sample bins
     # # log int:       Amount of 'log' to apply to the graph
     # # ordered bool:  Add an ordered histogram - show overall distribution
-    def section_byte_occurance_histogram(pebin, fig, ncols=2, ignore_0=True, bins=1, log=1, ordered=True):
+    def section_byte_occurrence_histogram(pebin, fig, ncols=2, ignore_0=True, bins=1, log=1, ordered=True):
 
         ignore_0 = int(ignore_0)
 
@@ -407,7 +415,7 @@ def section_graphs():
 
             ax.bar((range(ignore_0,256)), ordered_row, bins, color=c1, log=log, zorder=1)
 
-            # # Add a byte hist ordered by occurance - shows general distribution
+            # # Add a byte hist ordered by occurrence - shows general distribution
             if ordered:
                 sorted_row = []
                 c = Counter(section.content)
@@ -448,8 +456,8 @@ if __name__ == '__main__':
     parser.add_argument('--format', type=str, default=__figformat__, choices=['png', 'pdf', 'ps', 'eps','svg'], required=False, metavar='png', help='Graph output format')
     parser.add_argument('--figsize', type=int, nargs=2, default=__figsize__, metavar='#', help='Figure width and height in inches')
     parser.add_argument('--dpi', type=int, default=__figdpi__, metavar=__figdpi__, help='Figure dpi')
+    parser.add_argument('--showplt', action="store_true", default=__showplt__, help='Show plot interactively (disables saving to file)')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print debug information to stderr')
-
 
     subparsers = parser.add_subparsers(dest='graphtype', help='Graph type to generate')
     subparsers.required = True
@@ -474,7 +482,7 @@ if __name__ == '__main__':
 
     # # Set logging
     if args.verbose:
-        logging.basicConfig(stream=sys.stderr, format='Verbose | %(levelname)s | %(message)s', level=logging.DEBUG)
+        logging.basicConfig(stream=sys.stderr, format='Verbose | %(levelname)s | %(message)s', level=logging.INFO)
     else:
         logging.basicConfig(stream=sys.stderr, format='*** %(levelname)s | %(message)s', level=logging.CRITICAL)
 
@@ -487,7 +495,7 @@ if __name__ == '__main__':
         if not os.path.isfile(f):
             log.critical('Not a file, skipping: "{}"'.format(f))
         else:
-            log.debug('File exists: "{}"'.format(f))
+            log.info('File exists: "{}"'.format(f))
             files.append(f)
 
     # # Is the save_dir actually a dirctory?
@@ -496,7 +504,7 @@ if __name__ == '__main__':
         log.critical('--save_dir ({}), is not a directory...'.format(args.save_dir))
         exit(1)
     else:
-        log.debug('Saving graphs to directory "{}"'.format(args.save_dir))
+        log.info('Saving graphs to directory "{}"'.format(args.save_dir))
 
     # # Detect if all graphs are being requested + set required defaults
     graph_types = []
@@ -504,12 +512,13 @@ if __name__ == '__main__':
 
         graph_types.append('bin_ent')
         args.ibytes = __ibytes__
+        args.chunks = __chunks__
 
         graph_types.append('bin_hist')
         args.ignore_0 = __ignore_0__
         args.bins = __bins__
         args.log = __g_log__
-        args.ordered = __no_order__
+        args.no_order = __no_order__
 
     else:
         graph_types = [args.graphtype]
@@ -517,33 +526,33 @@ if __name__ == '__main__':
     # # Iterate over all given files
     for index, file in enumerate(files):
 
-        log.debug('+++ Processing: "{}"'.format(file))
+        log.info('+++ Processing: "{}"'.format(file))
 
         clean_fn = clean_fname(os.path.basename(file))
 
-        if len(args.file) > 0 and args.prefix:
-            save_fn = '{arg_prefix}-{clean_fn}-{{}}-{figsize}_{dpi}-{index}.{format}'.format(arg_prefix=args.prefix, clean_fn=clean_fn, figsize='x'.join(map(str, args.figsize)), dpi=args.dpi, index=index, format=args.format)
-        elif len(args.file) > 0 and not args.prefix:
-            save_fn = '{clean_fn}-{{}}-{figsize}_{dpi}-{index}.{format}'.format(clean_fn=clean_fn, figsize='x'.join(map(str, args.figsize)), dpi=args.dpi, index=index, format=args.format)
+        if len(args.file) > 1 and args.prefix:
+            save_fn = '{arg_prefix}-{clean_fn}-{{}}-{index}.{format}'.format(arg_prefix=args.prefix, clean_fn=clean_fn, index=index, format=args.format)
+        elif len(args.file) > 1 and not args.prefix:
+            save_fn = '{clean_fn}-{{}}-{index}.{format}'.format(clean_fn=clean_fn, index=index, format=args.format)
 
         elif len(args.file) == 1 and args.prefix:
-            save_fn = '{arg_prefix}.{format}'.format(arg_prefix=args.prefix, format=args.format)
+            save_fn = '{arg_prefix}-{{}}.{format}'.format(arg_prefix=args.prefix, format=args.format)
+        elif len(args.file) == 1 and args.prefix and len(graph_types) > 1:
+            save_fn = '{arg_prefix}-{{}}.{format}'.format(arg_prefix=args.prefix, format=args.format)
         elif len(args.file) == 1 and not args.prefix:
-            save_fn = '{clean_fn}-{{}}-{figsize}_{dpi}-{index}.{format}'.format(clean_fn=clean_fn, figsize='x'.join(map(str, args.figsize)), dpi=args.dpi, index=index, format=args.format)
+            save_fn = '{clean_fn}-{{}}.{format}'.format(clean_fn=clean_fn, format=args.format)
 
         save_fn = os.path.join(args.save_dir, save_fn)
 
         if 'bin_ent' in graph_types:
             __save_fn__ = save_fn.format('bin_ent')
-            log.debug('+ Generating bin_ent from "{}"'.format(file))
-            bin_ent(binname=file, frmt=args.format, figname=__save_fn__, figsize=(args.figsize[0], args.figsize[1]), figdpi=args.dpi, chunks=args.chunks, ibytes=json.loads(args.ibytes))
+            log.info('+ Generating bin_ent from "{}"'.format(file))
+            bin_ent(binname=file, frmt=args.format, figname=__save_fn__, figsize=(args.figsize[0], args.figsize[1]), figdpi=args.dpi, chunks=args.chunks, ibytes=json.loads(args.ibytes), showplt=args.showplt)
 
         if 'bin_hist' in graph_types:
             __save_fn__ = save_fn.format('bin_hist')
-            log.debug('+ Generating bin_hist from "{}"'.format(file))
-            bin_hist(binname=file, frmt=args.format, figname=__save_fn__, figsize=(args.figsize[0], args.figsize[1]), figdpi=args.dpi, ignore_0=args.ignore_0, bins=args.bins, g_log=args.log, ordered=args.no_order)
+            log.info('+ Generating bin_hist from "{}"'.format(file))
+            bin_hist(binname=file, frmt=args.format, figname=__save_fn__, figsize=(args.figsize[0], args.figsize[1]), figdpi=args.dpi, ignore_0=args.ignore_0, bins=args.bins, g_log=args.log, ordered=args.no_order, showplt=args.showplt)
 
-        log.debug('+++ Complete: "{}"'.format(file))
-
-
+        log.info('+++ Complete: "{}"'.format(file))
 
